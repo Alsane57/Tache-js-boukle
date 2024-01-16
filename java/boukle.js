939de03cf4d0663a53a7082139f21e25246d41c9
Alsane57 <136351035+Alsane57@users.noreply.github.com>
2024-01-16 15:38:46 +0100

    Add files via upload

---
+++ java/boukle.js
@@ -0,0 +1,17 @@
+// Boucle FOR pour afficher les nombres de 1 a 5
+
+for (let i = 1 ;  i<=5 ; i++ )
+console.log(i);
+
+
+//Boucle Do While pour afficher les nombres de 5 a 10
+
+let j = 1
+do {
+
+    console.log(j);
+
+    j++;
+
+} while (j <= 5);
+
